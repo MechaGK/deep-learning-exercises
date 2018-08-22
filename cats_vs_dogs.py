f13636da9f6c6dd58a1ba79bb493ed92f7f8736f
Abild Laptop <andreas@flexible.dk>
2018-08-22 14:40:42 +0200

    Add PY script and dataset

---
+++ cats_vs_dogs.py
@@ -0,0 +1,90 @@
+from keras.preprocessing.image import ImageDataGenerator 
+from keras.preprocessing import image
+from keras import Input, Model
+from keras.layers import Conv2D, Dense, MaxPool2D, Add, Flatten, Concatenate, Dropout
+import matplotlib.pyplot as plt
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+test_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_generator = train_datagen.flow_from_directory(
+    'train',
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+test_generator = train_datagen.flow_from_directory(
+    'test',
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary'
+)
+
+input_ = Input(shape=(150,150,3))
+conv_1 = Conv2D(64, (7, 7), activation='relu')(input_)
+max_1 = MaxPool2D((2,2))(conv_1)
+conv_2 = Conv2D(128, (5, 5), activation='relu')(max_1)
+max_2 = MaxPool2D((2,2))(conv_2)
+conv_3 = Conv2D(256, (3,3), activation='relu')(max_2)
+max_3 = MaxPool2D((2,2))(conv_3)
+conv_4 = Conv2D(512, (3,3), activation = 'relu')(max_3)
+max_4 = MaxPool2D((3,3))(conv_4)
+conv_5 = Conv2D(2048, (4,4), activation = 'relu')(max_4) 
+flatten_ = Flatten()(conv_5)
+dense_1 = Dense(1024, activation = 'relu')(flatten_)
+dense_2 = Dense(512, activation = 'relu')(dense_1)
+dense_3 = Dense(128, activation = 'relu')(dense_2)
+concat_ = Concatenate()([dense_3, flatten_])
+out = Dense(1, activation = 'sigmoid')(concat_)
+
+model = Model(inputs=input_, outputs=out)
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch = 100,
+    epochs = 50,
+    validation_data=test_generator,
+    validation_steps=50
+)
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values) + 1)
+
+
+# ’bo’ is for blue dot, ‘b’ is for solid blue line
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+model.save('my_model.h5')
